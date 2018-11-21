0df6442faf8b69ffb01d97a03a85830c56a69c92
Sun_Roy <wangleiandroy@icloud.com>
2018-11-21 09:33:10 +0800

    Initial Commit

---
+++ Management/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Management
+//
+//  Created by 王雷 on 2018/11/21.
+//  Copyright © 2018 Roy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
